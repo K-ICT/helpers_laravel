9d223c174c07b7a3e0b8be8f2627eddffb39834a
Ahmad Fazli Ismail <fazli@cloud.org.my>
2021-02-28 23:29:33 +0800

    Accept additional argument for record summary path prefix at src/App/Helpers/InitModuleRecordSummary.php

--- src/App/Helpers/InitModuleRecordSummary.php
+++ src/App/Helpers/InitModuleRecordSummary.php
@@ -6,16 +6,17 @@ if(!function_exists('initModuleRecordSummary'))
      * Instantiate the requested module record summary for a specific category.
      *
      * @param  string  $moduleName
+     * @param  string  $recordSummaryPathPrefix
      * @return object
      */
-    function initModuleRecordSummary(string $moduleName = null)
+    function initModuleRecordSummary(string $moduleName = null, string $recordSummaryPathPrefix = null)
     {
         $request = request();
 
         $summaryCategory = $request->summarize;
 
         # Define the provided category to be summarized.
-        $summaryCategory = '\Kict\FpxExchangeLaravel\App\RecordSummaries\\' .str_plural($moduleName) .'\\' .ucwords(camel_case($summaryCategory));
+        $summaryCategory = $recordSummaryPathPrefix .str_plural($moduleName) .'\\' .ucwords(camel_case($summaryCategory));
 
         # Instantiate the requested search result summary category / what to summarize.
         $summaryCategoryClass = new $summaryCategory;
